17fa44b6674030b2ef29bed27584d88f1ec069f3
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2022-11-09 08:59:52 +0900

    Add header

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Header from '@components/Header'
+import Banner from '@components/Banner'
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       </Head>
 
       <Header />
+      <Banner />
       <p className="font-bold text-9xl flex justify-center items-center h-screen">
         Hello World
       </p>
