3dd5e0f4be467e477383c7b1d66074b5b91ee81e
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2022-11-09 20:28:09 +0900

    Small Card layout

--- components/SmallCard.tsx
+++ components/SmallCard.tsx
@@ -8,8 +8,15 @@ interface Props {
 
 const SmallCard: NextPage<Props> = ({ spot }) => {
   return (
-    <div className="relative">
-      <Image src={spot.img} alt="img" layout="fill" />
+    <div className="flex items-center m-2 mt-5 space-x-4 rounded-xl cursor-pointer hover:bg-gray-100 hover:scale-105 transition transform duration-200 ease-out">
+      <div className="relative w-16 h-16">
+        <Image src={spot.img} alt="img" layout="fill" className="rounded-lg" />
+      </div>
+
+      <div>
+        <h2>{spot.location}</h2>
+        <h3 className="text-gray-500">{spot.distance}</h3>
+      </div>
     </div>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -39,9 +39,12 @@ export default function Home({ spots }: Props) {
       <main className="max-w-7xl mx-auto px-8 sm:px-16">
         <section className="pt-6">
           <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
-          {spots?.map((spot, idx) => (
-            <SmallCard spot={spot} key={idx} />
-          ))}
+
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+            {spots?.map((spot, idx) => (
+              <SmallCard spot={spot} key={idx} />
+            ))}
+          </div>
         </section>
       </main>
     </div>
